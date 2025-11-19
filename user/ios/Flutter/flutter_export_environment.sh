d7f9c54b8ec262115a9dc555f367cfc3e3e257a4
Julián Molina <phalcondroid@gmail.com>
2025-11-19 01:27:50 -0500

    update user

---
+++ user/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/julianmolina/Libraries/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/julianmolina/Documents/projects/mobile/dragonfly/user"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/julianmolina/Documents/projects/mobile/dragonfly/user/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/julianmolina/Documents/projects/mobile/dragonfly/user/.dart_tool/package_config.json"
